f0adce6d5a3bd90b3a9c7566deb51294dc0be7ad
David Brokaw <dgbrokaw@indiana.edu>
2016-03-30 15:02:46 -0400

    removed border portion of person svg; translate person definition portion to the left to make up for absence of border; add jank extra space to svg to make up for imperfections in pixel size of people; fix bug in getPeepAndTally search

--- peep-grid.js
+++ peep-grid.js
@@ -2,7 +2,7 @@ var PeepGrid = (function() {
 	var PeepGrid = function(container, options) {
 		this.container = d3.select(container);
 		this.options = extend(PeepGrid.defaultOptions, options);
-		this.personSize = {width: 118, height: 118}
+		this.personSize = {width: 44, height: 102}
 		this.tally = 0;
 		this.initSVGAndDefs();
 		this.initPeeps();
@@ -15,14 +15,18 @@ var PeepGrid = (function() {
 	}
 
 	PeepGrid.defaultOptions = {
-		rows: 10
-	, peoplePerRow: 10
-	, scale: 0.75
+		rows: 5
+	, peoplePerRow: 20
+	, scale: 1
+	, margin_right: 10
+	, margin_bottom: 10
 	}
 
 	PeepGrid.prototype.initSVGAndDefs = function() {
-		this.svgSize = { width: this.options.peoplePerRow * this.personSize.width
-		               , height: this.options.rows * this.personSize.height }
+		this.svgSize = { width: (this.options.peoplePerRow * this.personSize.width) + 2
+		               , height: (this.options.rows * this.personSize.height) + 12 }
+		// this.svgSize.width += (this.options.peoplePerRow - 1) * this.options.margin_right;
+		// this.svgSize.height += (this.options.ro
 
 		this.svg = this.container.append('svg')
 			.attr('width', this.svgSize.width*this.options.scale)
@@ -35,18 +39,20 @@ var PeepGrid = (function() {
 			.attr('id', 'person');
 
 		// background (not visible)
-		this.personDef.append('path')
-			.attr('d', "M13.945,1.91c-6.758,0-12.25,5.5-12.25,12.258v94.039c0,6.734,5.492,12.227,12.25,12.227h94.023c6.766,0,12.258-5.492,12.258-12.227V14.168c0-6.758-5.492-12.258-12.258-12.258H13.945z")
-			.attr('fill', '#FFFFFF');
+		// this.personDef.append('path')
+		// 	.attr('d', "M13.945,1.91c-6.758,0-12.25,5.5-12.25,12.258v94.039c0,6.734,5.492,12.227,12.25,12.227h94.023c6.766,0,12.258-5.492,12.258-12.227V14.168c0-6.758-5.492-12.258-12.258-12.258H13.945z")
+		// 	.attr('fill', '#FFFFFF');
 
 		// head of the person
 		this.personDef.append('path')
 			.attr('d', "M60.727,27.41c4.555,0,8.273-3.688,8.273-8.273c0-4.57-3.719-8.281-8.273-8.281c-4.57,0-8.281,3.711-8.281,8.281C52.445,23.723,56.156,27.41,60.727,27.41z")
+			.attr('transform', 'translate(-37,0)')
 			.attr({stroke: '#000000', 'stroke-width': '2px'});
 
 		// body of the person
 		this.personDef.append('path')
 			.attr('d', "M49.484,107.762c0,2.562,2.086,4.664,4.648,4.664s4.648-2.102,4.648-4.664V68.793h3.898v38.969c0,2.562,2.062,4.664,4.625,4.664s4.648-2.102,4.648-4.664l0.039-67.18h3.859v24.766c0,4.992,6.484,4.992,6.461,0V40.035c0-5.492-4.25-10.867-10.688-10.867l-21.992-0.039c-5.859,0-10.578,4.812-10.578,10.758v25.461c0,4.938,6.531,4.938,6.531,0V40.582h3.93L49.484,107.762z")
+			.attr('transform', 'translate(-37,0)')
 			.attr({stroke: '#000000', 'stroke-width': '2px'});
 	}
 
@@ -92,7 +98,7 @@ var PeepGrid = (function() {
 
 	PeepGrid.prototype.getPeepAndTally = function(x, y) {
 		var r_x = Math.max(0, Math.min(Math.floor(x/(this.personSize.width)), this.grid[0].length-1))
-		  , r_y = Math.max(0, Math.min(Math.floor(y/(this.personSize.width)), this.grid.length-1));
+		  , r_y = Math.max(0, Math.min(Math.floor(y/(this.personSize.height)), this.grid.length-1));
 		var peep = this.grid[r_y][r_x]
 		if (peep) {
 			this.tally++;
